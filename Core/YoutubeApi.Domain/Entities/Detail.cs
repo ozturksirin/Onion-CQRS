f49689af121f6f033570a13d2e59cf72203c9d9c
ozturk <ozturksirininfo@gmail.com>
2024-08-28 01:04:47 +0300

    Refactor entities and add UnitOfWork implementation
    Removed `Class1` from various namespaces.
    Updated `Brand`, `Category`, `Detail`, and `Product` classes in `YoutubeApi.Domain.Entities` by removing `required` keywords from several properties.
    Added `IUnitOfWork` interface in `YoutubeApi.Application.Interfaces.UnitOfWorks`.
    Implemented `UnitOfWork` class in `YoutubeApi.Presistence.UnitOfWorks`.
    Updated `Registration` class to include `IUnitOfWork` and `UnitOfWork` in DI container.

--- Core/YoutubeApi.Domain/Entities/Detail.cs
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -22,10 +22,10 @@ namespace YoutubeApi.Domain.Entities
             CategoryId = categoryId;
         }
 
-        public required string Title { get; set; }
-        public required string Description { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
 
-        public required int CategoryId { get; set; }
+        public int CategoryId { get; set; }
 
         public Category Category { get; set; }
     }
